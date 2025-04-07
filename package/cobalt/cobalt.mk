2e645280f5bc0ed921a6563edf42bf3756dc303a
Neeraj Deshpande <30022081+neerajrd@users.noreply.github.com>
2025-04-07 22:10:55 +0530

    Added raspberrypi3 defconfig for cobalt compilation & cobalt package stub.

---
+++ package/cobalt/cobalt.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# COBALT
+#
+################################################################################
+
+COBALT_VERSION = 01e0507bff2dcba83fffc0ef6f21d4d1b7d5f6bc
+COBALT_SITE_METHOD = git
+COBALT_SITE = git@github.com:krivision/cobalt
+COBALT_INSTALL_STAGING = YES
+COBALT_DEPENDENCIES = host-cmake host-ninja host-python3 host-python-six host-gn ffmpeg
+
+define COBALT_BUILD_CMDS
+endef
+
+define COBALT_INSTALL_STAGING_CMDS
+endef
+
+define COBALT_INSTALL_TARGET_CMDS
+endef
+
+$(eval $(generic-package))
