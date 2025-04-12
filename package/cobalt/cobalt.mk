6500b7fe8a4e9aab09b0bbb2d0e0d96458ceb224
Neeraj Deshpande <30022081+neerajrd@users.noreply.github.com>
2025-04-12 12:15:48 +0530

    Added host-nodejs dependency for cobalt to compile in qa mode.

--- package/cobalt/cobalt.mk
+++ package/cobalt/cobalt.mk
@@ -8,7 +8,7 @@ COBALT_VERSION = 01e0507bff2dcba83fffc0ef6f21d4d1b7d5f6bc
 COBALT_SITE_METHOD = git
 COBALT_SITE = git@github.com:krivision/cobalt
 COBALT_INSTALL_STAGING = YES
-COBALT_DEPENDENCIES = host-cmake host-ninja host-python3 host-python-six host-gn ffmpeg
+COBALT_DEPENDENCIES = host-cmake host-ninja host-python3 host-python-six host-nodejs host-gn ffmpeg
 
 define COBALT_BUILD_CMDS
 endef
